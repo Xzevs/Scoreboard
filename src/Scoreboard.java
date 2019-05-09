2603601d8da86901489daa4d2f0c928faf2458d2
rasmus.karlsson11 <rasmus.karlsson@elev.ga.ntig.se>
2019-05-09 10:41:30 +0200

    Scoreboard

---
+++ src/Scoreboard.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Scoreboard {
+    public static void main(String[]args){
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> scores = new ArrayList();
+        int score;
+        do{
+            System.out.println("Input next score (-1 to quit)):");
+            score = in.nextInt();
+            if (score !=1);{
+                boolean index;
+                scores.add(index?,score);
+
+        }
+    } while (score !=-1);
+}
